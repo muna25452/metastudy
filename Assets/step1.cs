9c637a64343c3d56bd1e8377814326119ce7f8ea
sono <dkakal123@naver.com>
2023-11-12 16:18:50 +0900

    testcomp

---
+++ Assets/step1.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class step1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int myint = 10;
+        double mydouble = 10.0;
+        float myfloat = 1.0f;
+        bool mybool = false;
+        string mystring = "гоюл";
+        Debug.Log(myint);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
